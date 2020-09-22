5c6f2b0052ff6fa3eb5116ffad1a8b837c82f940
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-09-22 10:45:20 -0500

    Added JS code

---
+++ script.js
@@ -0,0 +1,38 @@
+let now = new Date();
+
+let currentDate = document.querySelector("#time");
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function newCityName(event) {
+  event.preventDefault();
+  let apiKey = `06c11d11419debba7ff8c2da3a596e82`;
+  let city = document.querySelector("#search-form-value").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", newCityName);
