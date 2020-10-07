4eae5726b395557521045d21e245e7ad0794bfe3
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-10-07 15:51:43 -0500

    Troubleshoot wind speed

--- src/index.js
+++ src/index.js
@@ -25,11 +25,12 @@ function displayWeather(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
   iconImage.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 function newCityName(event) {
